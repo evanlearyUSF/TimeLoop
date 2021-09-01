cdfbee96e5b8d4827f29fb596bdac4c4d9cbb379
Evan Leary <evanleary@usf.edu>
2021-08-31 20:42:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine(i + " " + "Abracadabra");
+            }
+
+            // we also came up with a solution using while loop
+            // setting int orderCounter = 1 & while
+            // x > orderCounter using the console.Writeline
+            // and adding an orderCounter ++ clause
+            // to count up to the X by incrementing OrderCounter
+
+
+        }
+    }
+}
